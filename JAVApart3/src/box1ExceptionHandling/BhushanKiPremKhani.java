efb40abcf6955f3152e3ff9a872583d51bb74862
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-09 21:09:16 +0530

    try and catch block

--- JAVApart3/src/box1ExceptionHandling/BhushanKiPremKhani.java
+++ JAVApart3/src/box1ExceptionHandling/BhushanKiPremKhani.java
@@ -11,7 +11,13 @@ public class BhushanKiPremKhani {
 		int n1 = sc.nextInt();
 		System.out.println("Enter 2nd Number :-");
 		int n2 = sc.nextInt();
-		System.out.println(n1 / n2);
+
+		try {
+			System.out.println(n1 / n2);
+		} catch (ArithmeticException a1) {
+			System.out.println("Gadhe zero se divide mat kar");
+		}
+
 		System.out.println("Dombivali");
 		System.out.println("main end");
 	}
