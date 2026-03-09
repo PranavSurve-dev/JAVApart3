71d7bd0af82dfcaf739e4d98c9c797467dbfad38
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-09 20:42:44 +0530

    Exception Handling

---
+++ JAVApart3/src/box1ExceptionHandling/BhushanKiPremKhani.java
@@ -0,0 +1,16 @@
+package box1ExceptionHandling;
+
+import java.util.Scanner;
+
+public class BhushanKiPremKhani {
+	public static void main(String[] args) {
+		System.out.println("Bhiwandi");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st Number :-");
+		int n1 = sc.nextInt();
+		System.out.println("Enter 2nd Number :-");
+		int n2 = sc.nextInt();
+		System.out.println(n1 / n2);
+		System.out.println("Dombivali");
+	}
+}
