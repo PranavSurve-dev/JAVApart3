88b29cedb39009817f3e84589d34ea67d49cfaa6
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-14 19:47:29 +0530

    practice

--- JAVApart3/src/box1ExceptionHandling/Degree.java
+++ JAVApart3/src/box1ExceptionHandling/Degree.java
@@ -12,11 +12,26 @@ public class Degree {
 		}
 	}
 
+	public static void car(int age) throws Z2Exception {
+		if (age > 21) {
+			System.out.println("now u can drive me......");
+		} else {
+			throw new Z2Exception();
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Code Start");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your Marks");
 		int marks = sc.nextInt();
+		System.out.println("Enter your age");
+		int age = sc.nextInt();
+		try {
+			car(age);
+		} catch (Z2Exception z2) {
+			System.out.println("wait for some time...");
+		}
 		try {
 			pass(marks);
 		} catch (Z2Exception z1) {
@@ -26,6 +41,7 @@ public class Degree {
 			System.out.println("All The Best...");
 			sc.close();
 		}
+
 		System.out.println("Code End");
 	}
 }
--- JAVApart3/src/box1ExceptionHandling/Z2Exception.java
+++ JAVApart3/src/box1ExceptionHandling/Z2Exception.java
@@ -3,5 +3,5 @@ package box1ExceptionHandling;
 //Custom Checked Exception
 public class Z2Exception extends RuntimeException {
 
-//nothing
+	// nothing
 }
