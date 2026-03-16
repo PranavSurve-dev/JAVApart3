2edb154ecab26586fbc6f78276ee6bdc4bd9f6d1
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-16 21:05:48 +0530

    toString()

---
+++ JAVApart3/src/box1ExceptionHandling/Train.java
@@ -0,0 +1,32 @@
+package box1ExceptionHandling;
+
+//1 Before Overriding toString()
+class VandeBharat {
+	String color;
+	int price;
+	int speed;
+
+	public VandeBharat(String color, int price, int speed) {
+
+		this.color = color;
+		this.price = price;
+		this.speed = speed;
+	}
+
+}
+
+public class Train {
+	public static void main(String[] args) {
+
+		VandeBharat v1 = new VandeBharat("Orange", 1500, 180);
+		System.out.println(v1);// box1ExceptionHandling.VandeBharat@6f539caf
+		System.out.println(v1.toString());// box1ExceptionHandling.VandeBharat@6f539caf
+
+		System.out.println("--------------");
+
+		VandeBharat v2 = new VandeBharat("Red", 1800, 150);
+		System.out.println(v2);// box1ExceptionHandling.VandeBharat@79fc0f2f
+		System.out.println(v2.toString());// box1ExceptionHandling.VandeBharat@79fc0f2f
+
+	}
+}
