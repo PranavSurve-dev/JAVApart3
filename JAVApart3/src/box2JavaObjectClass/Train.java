94a6c9ee960ca88435aac45c0bb894973030418e
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-17 11:28:06 +0530

    Rewrite

--- JAVApart3/src/box2JavaObjectClass/Train.java
+++ JAVApart3/src/box2JavaObjectClass/Train.java
@@ -1,32 +1,70 @@
+//package box2JavaObjectClass;
+//
+////1 Before Overriding toString()
+//class VandeBharat {
+//	String color;
+//	int price;
+//	int speed;
+//
+//	public VandeBharat(String color, int price, int speed) {
+//
+//		this.color = color;
+//		this.price = price;
+//		this.speed = speed;
+//	}
+//
+//}
+//
+//public class Train {
+//	public static void main(String[] args) {
+//
+//		VandeBharat v1 = new VandeBharat("Orange", 1500, 180);
+//		System.out.println(v1);// box1ExceptionHandling.VandeBharat@6f539caf
+//		System.out.println(v1.toString());// box1ExceptionHandling.VandeBharat@6f539caf
+//
+//		System.out.println("--------------");
+//
+//		VandeBharat v2 = new VandeBharat("Red", 1800, 150);
+//		System.out.println(v2);// box1ExceptionHandling.VandeBharat@79fc0f2f
+//		System.out.println(v2.toString());// box1ExceptionHandling.VandeBharat@79fc0f2f
+//
+//	}
+//}           
+
 package box2JavaObjectClass;
 
-//1 Before Overriding toString()
+// Class
 class VandeBharat {
-	String color;
+	String colour;
 	int price;
 	int speed;
 
-	public VandeBharat(String color, int price, int speed) {
-
-		this.color = color;
+	// Constructor
+	public VandeBharat(String colour, int price, int speed) {
+		this.colour = colour;
 		this.price = price;
 		this.speed = speed;
 	}
 
+	// 🔥 Overriding toString()
+	@Override
+	public String toString() {
+		return "Train colour is " + colour + ", price is " + price + ", speed is " + speed;
+	}
 }
 
+// Main class
 public class Train {
 	public static void main(String[] args) {
 
-		VandeBharat v1 = new VandeBharat("Orange", 1500, 180);
-		System.out.println(v1);// box1ExceptionHandling.VandeBharat@6f539caf
-		System.out.println(v1.toString());// box1ExceptionHandling.VandeBharat@6f539caf
+		VandeBharat v1 = new VandeBharat("White", 1500, 180);
+		System.out.println(v1); // calls toString()
+		System.out.println(v1.toString()); // explicit call
 
 		System.out.println("--------------");
 
 		VandeBharat v2 = new VandeBharat("Red", 1800, 150);
-		System.out.println(v2);// box1ExceptionHandling.VandeBharat@79fc0f2f
-		System.out.println(v2.toString());// box1ExceptionHandling.VandeBharat@79fc0f2f
-
+		System.out.println(v2);
+		System.out.println(v2.toString());
 	}
 }
