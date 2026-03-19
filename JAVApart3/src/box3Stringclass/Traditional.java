329805fd99dd72277b648ca0f580f9a5975600a6
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-19 20:24:34 +0530

    StringBuffer

---
+++ JAVApart3/src/box3Stringclass/Traditional.java
@@ -0,0 +1,24 @@
+package box3Stringclass;
+
+public class Traditional {
+
+	public static void main(String[] args) {
+		// String is immutable
+
+		String s1 = new String("Pranav");
+		s1.concat("muskan");
+		System.out.println(s1);// Pranav
+
+		System.out.println("-------------");
+
+		// Storing in different variable to show concat method works
+		String str = "Abhijit".concat("Muskan");
+		System.out.println(str);
+		System.out.println("-------------");
+		// Mutable
+		StringBuffer sb = new StringBuffer("Pushparaj");
+		sb.append("Srivalli");
+		System.out.println(sb);
+		System.out.println("--------------");
+	}
+}
