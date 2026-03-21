0bab87da458b7504ed6c155dd6a4f4f1c407e93a
PranavSurve-dev <pranavsurve444@gmail.com>
2026-03-21 20:30:44 +0530

    split()

--- JAVApart3/src/box3Stringclass/BlackWindow.java
+++ JAVApart3/src/box3Stringclass/BlackWindow.java
@@ -1,5 +1,7 @@
 package box3Stringclass;
 
+import java.util.Arrays;
+
 public class BlackWindow {
 	public static void main(String[] args) {
 		String str = "Java Developer";
@@ -8,5 +10,19 @@ public class BlackWindow {
 		String s1 = "Chamtkar pe Chamtkar kr aa rhe hai";
 		System.out.println(s1.replace("Chamtkar", "Balatakar"));
 		// Balatakar pe Balatakar kr aa rhe hai
+
+		System.out.println("-----------------------");
+
+		System.out.println(Arrays.toString(s1.split(" ")));
+
+		String[] arr = s1.split(" ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+			System.out.println("--------------");
+			// for each loop
+			for (String n : arr) {
+				System.out.println(n);
+			}
+		}
 	}
 }
