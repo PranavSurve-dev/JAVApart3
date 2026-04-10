1aeb0862bd8a1ef6418e06dab9210fb4e523aa44
PranavSurve-dev <pranavsurve444@gmail.com>
2026-04-10 21:25:48 +0530

    servlet

--- 02-Servlet-Project/src/main/java/com/tyss/servlet/MyServlet.java
+++ 02-Servlet-Project/src/main/java/com/tyss/servlet/MyServlet.java
@@ -7,7 +7,9 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet("/servlet")
 public class MyServlet extends GenericServlet {
 
 	@Override
